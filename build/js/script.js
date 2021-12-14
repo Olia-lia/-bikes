12ea18e080cae67451ade64302c34502c58a3663
Olga Lee <olyalee100@gmail.com>
2021-12-15 02:27:50 +0300

    added header

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,33 @@
 'use strict';
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
+var mainNav = document.querySelector('.main-nav');
+
+var form = document.querySelector('.form');
+var phoneInput = form.querySelector('.form__input--phone');
 
 pageHeader.classList.remove('page-header--nojs');
+pageHeader.classList.add('page-header--closed-menu');
+mainNav.classList.remove('main-nav--nojs');
 
 headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+  if (headerToggle.classList.contains('toggle-navigation--open')) {
+    headerToggle.classList.remove('toggle-navigation--open');
+    headerToggle.classList.add('toggle-navigation--close');
+    pageHeader.classList.remove('page-header--closed-menu');
+    pageHeader.classList.add('page-header--opened-menu');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    headerToggle.classList.add('toggle-navigation--open');
+    headerToggle.classList.remove('toggle-navigation--close');
+    pageHeader.classList.add('page-header--closed-menu');
+    pageHeader.classList.remove('page-header--opened-menu');
   }
 });
+
+const checkPhoneInputValidity = () => {
+  const valuePhoneInput = phoneInput.value;
+console.log(valuePhoneInput)
+}
+
+
+checkPhoneInputValidity();
